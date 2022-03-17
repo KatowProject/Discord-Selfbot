4a9453177484d0c3c888eafbfb65204844116556
KatowProject <naufalfaqih443@gmail.com>
2022-03-17 15:07:00 +0700

    partial update

--- index.js
+++ index.js
@@ -1,42 +1,42 @@
+const HTTP = require('./HTTP.js');
+const UAParser = require('ua-parser-js');
 const axios = require('axios');
-
 class Discord {
-    constructor(conf) {
-        if (typeof conf !== 'object') throw new Error('Config is not an object');
-        if (typeof conf.token !== 'string') throw new Error('Token is not a string');
-        if (typeof conf.api !== 'string' && !conf.api.includes('https://')) throw new Error('API is not a string');
-
-        this.api = conf.api;
-        this.token = { headers: { Authorization: `${conf.token}` } };
+    constructor(token, opt) {
+        if (typeof token === 'undefined') throw new Error('Token is not a string or object');
+        this.tokens = token;
+        this.options = opt;
     }
 
-    async getMessages(channel, limit = 10) {
-        try {
-            const url = this.api + `/channels/${channel}/messages?limit=${limit}`;
-            const response = await axios.get(url, this.token);
+    async init(f) {
+        const createDevice = async () => {
+            const UAs = await axios.get('https://jnrbsn.github.io/user-agents/user-agents.json').then(res => res.data);
+            const distok = await axios.get('http://distok.top/state.json').then(res => res.data);
 
-            return response.data;
-        } catch (err) {
-            throw new Error(err);
+            const UserAgent = new UAParser(UAs[Math.floor(Math.random() * UAs.length)]);
+            this.device = {
+                os: UserAgent.getOS().name,
+                browser: UserAgent.getBrowser().name,
+                device: UserAgent.getDevice().name || '',
+                system_locale: 'en-US',
+                browser_user_agent: UserAgent.getUA(),
+                browser_version: UserAgent.getBrowser().version,
+                os_version: UserAgent.getOS().version,
+                referrer: '',
+                referring_domain: '',
+                referrer_current: '',
+                referring_domain_current: '',
+                release_channel: 'stable',
+                client_build_number: 119446,
+                client_event_source: null
+            }
+
+            this.request = new HTTP(this);
+            this.loaded = true;
         }
-    }
 
-    async sendMessage(channel, message) {
-        try {
-            const url = this.api + `/channels/${channel}/messages`;
-            const response = await axios.post(url, { content: message }, this.token);
-
-            return response.data;
-        } catch (err) {
-            //get status text
-            throw new Error('Error: ' + err.response.statusText);
-        }
-    }
-
-    changeToken(token) {
-        if (typeof token !== 'string') throw new Error('Token is not a string');
-
-        this.token = { headers: { Authorization: `${token}` } };
+        if (f === true || !this.loaded) return await createDevice();
+        if (this.loaded) throw new Error('Already loaded');
     }
 }
 
--- test.js
+++ test.js
@@ -1,37 +1,11 @@
-const Discord = require('./index.js');
-const client = new Discord({ api: 'https://discord.com/api/v9', token: 'token' });
-
-
-// Test 1: getMessages
-const channel = '932997960923480101';
-const limit = 10;
-const messageid = '953699252012462090';
+const Discord = require('./index');
+const client = new Discord(['token1', 'token2']);
 
 (async () => {
-    try {
-        // client.sendMessage(channel, 'Hello World!');
-        // client.getMessages(channel, limit);
-        const msg = await client.getMessages(channel, limit);
-        const message = msg.find(m => m.id === messageid);
+    await client.init(true);
 
-        const embed = message.embeds[0];
-        const description = embed.description;
+    const request = client.request;
 
-        const content = description.split('\n');
-        const xps = content.map(c => {
-            const userID = c.split('[')[1].split(']')[0];
-            let xp = c.split('`')[1];
-            if (xp === 'NaN') xp = 0;
-            return `!give-xp <@${userID}> ${parseInt(xp)}`;
-        })
-
-        for (xp of xps) {
-            client.sendMessage('932997960923480099', xp);
-
-            //delay 3s
-            await new Promise(resolve => setTimeout(resolve, 3000));
-        }
-    } catch (err) {
-        console.log(err);
-    }
+    const sendMessage = await request.sendMessage('932997960923480099', 'Hello World!', true);
+    console.log(sendMessage);
 })()
